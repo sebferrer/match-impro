bcedb601701f1e04826d4b16bf38e51da2af95ae
sebferrer <sebastien@sebferrer.fr>
2024-01-28 18:14:10 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './ui/home';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent
+	],
+	imports: [
+		CommonModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		RouterModule,
+		MatTooltipModule,
+	],
+	providers: [
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/ui/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+// import * as TECHNOS_JSON from 'src/app/infra/static-technos.json';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+	constructor(
+	) { }
+
+	public ngOnInit(): void {
+
+	}
+}
