82bc4cd5761cefb4b65983d450c4c9f56a361e51
sebferrer <sebastien@sebferrer.fr>
2024-02-10 01:28:26 +0100

    feat(slides): slides rolling

--- src/app/ui/slides/slides.component.ts
+++ src/app/ui/slides/slides.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-// import * as TECHNOS_JSON from 'src/app/infra/static-technos.json';
 
-const SLIDES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const SLIDES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+	'11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+	'21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+	'31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+	'41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
+	'51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
+	'61', '62', '63', '64'];
 
 @Component({
 	selector: 'app-slides',
@@ -9,6 +14,7 @@ const SLIDES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 	styleUrls: ['./slides.component.scss']
 })
 export class SlidesComponent implements OnInit {
+	slidesBank: string[];
 	slides: string[];
 	size: number;
 	currentSlideIndex: number;
@@ -19,9 +25,19 @@ export class SlidesComponent implements OnInit {
 	}
 
 	public ngOnInit(): void {
-		this.slides = this.shuffleArray(SLIDES)
+		this.slidesBank = [...SLIDES];
+		this.initSlides();
+	}
+
+	public initSlides(): void {
+		this.slides = this.shuffleArray(this.slidesBank)
 		this.currentSlideIndex = 0;
 		this.currentSlide = '0';
+		this.slidesBank = this.slidesBank.filter(n => !this.slides.includes(n));
+	}
+
+	public enoughSlides(): boolean {
+		return this.slidesBank.length >= 7;
 	}
 
 	public changeSlide(event: any, direction: boolean): void {
@@ -39,11 +55,11 @@ export class SlidesComponent implements OnInit {
 			}
 			this.currentSlideIndex--;
 		}
-		if(this.currentSlideIndex === 0) {
+		if (this.currentSlideIndex === 0) {
 			this.currentSlide = '0';
 			return
 		}
-		this.currentSlide = this.slides[this.currentSlideIndex-1];
+		this.currentSlide = this.slides[this.currentSlideIndex - 1];
 	}
 
 	private shuffleArray(array: any): string[] {
